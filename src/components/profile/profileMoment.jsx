03a6df79621f4549ab42b3d4dd602ffa23583f49
kimgyul11 <coloo88@naver.com>
2023-10-15 16:48:26 +0900

    프로필 디자인,기능 구현완료

---
+++ src/components/profile/profileMoment.jsx
@@ -0,0 +1,70 @@
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 100%;
+  height: 65px;
+  padding: 6px;
+  border-bottom: 1px solid #e0e0e0;
+  margin-bottom: 6px;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  &:hover {
+    background: #e0e0e0;
+    border-radius: 5px;
+  }
+`;
+const Header = styled.div`
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
+  p {
+    color: #888888;
+    font-size: 0.7rem;
+    margin-left: 4px;
+  }
+`;
+
+const Profile = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  img {
+    width: 20px;
+    height: 20px;
+    border-radius: 999px;
+    border: 1px solid #e0e0e0;
+  }
+`;
+
+const Content = styled.p`
+  flex: 1;
+  padding: 6px;
+  font-size: 0.8rem;
+  font-weight: 700;
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export default function ProfileMomentBox({ moment }) {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate(`/moment/${moment.id}`);
+  };
+  return (
+    <Box onClick={onClick}>
+      <Header>
+        <Profile>
+          <img src={moment.userPhoto ? moment.userPhoto : ""} />
+          <p>{moment.username}</p>
+        </Profile>
+        <p>{dayjs(moment.createdAt).format("YYYY년 MM월 DD일 HH:mm")}</p>
+      </Header>
+      <Content>{moment.text}</Content>
+    </Box>
+  );
+}
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -14,6 +14,9 @@ import {
   where,
 } from "firebase/firestore";
 
+import ProfileMomentBox from "../../components/profile/profileMoment";
+import classes from "../profile/Profile.module.css";
+
 const Wrap = styled.div`
   width: 100%;
   display: flex;
@@ -21,32 +24,120 @@ const Wrap = styled.div`
   justify-content: center;
 `;
 const ProfileWrap = styled.div`
+  display: flex;
   width: 80%;
   height: 80vh;
-  background: beige;
+  @media (max-width: 800px) {
+    flex-direction: column;
+    width: 100%;
+  }
 `;
 const LeftInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 50%;
   height: 100%;
+  padding: 12px;
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
+const ProfileInfo = styled.div`
+  text-align: center;
+  margin-top: 30px;
+  height: 200px;
 
-  background-color: #b9b9a9;
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 999px;
+  }
+  p {
+    margin-top: 10px;
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+  button {
+    margin-top: 6px;
+    padding: 6px;
+    border-radius: 999px;
+    border: 1px solid #66b7d7;
+    color: #66b7d7;
+    background: #fff;
+    font-weight: 600;
+    cursor: pointer;
+    &:hover {
+      background: #66b7d7;
+      color: #fff;
+    }
+  }
 `;
-const Img = styled.img`
-  width: 150px;
-  border-radius: 999px;
-  cursor: pointer;
+const ProfileMoment = styled.div`
+  flex: 1;
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #e0e0e0;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
-const Info = styled.div``;
+
+const Title = styled.h3`
+  width: 100%;
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-bottom: 12px;
+`;
+
+const RightInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  width: 50%;
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
+
+const Tab = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+  width: 100%;
+  height: 50px;
+
+  p {
+    width: 150px;
+    height: 30px;
+    margin: 12px;
+    border-radius: 999px;
+    border: 1px solid #e0e0e0;
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+`;
+const MomentWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 const Profile = () => {
   const [likeMoment, setLikeMoment] = useState([]);
   const [myMoments, setMyMoments] = useState([]);
+  const [followingMoments, setFollowingMoments] = useState([]);
+  const [followingIds, setFollowingIds] = useState([""]);
   const user = auth.currentUser;
-  console.log(likeMoment, myMoments);
 
   const [displayName, setDisplayName] = useState(user.displayName || "사용자");
   const [userPhoto, setUserPhoto] = useState(user.photoURL);
   const [newDisplayName, setNewDisplayName] = useState(displayName);
+  const [activeTab, setActiveTab] = useState("like");
   const [isEdit, setIsEdit] = useState(false);
   const onChange = (e) => {
     const {
@@ -105,8 +196,29 @@ const Profile = () => {
     }
   };
 
+  //팔로잉하는 유저 가져오기
+  const getFollowingIds = useCallback(() => {
+    if (user.uid) {
+      const ref = doc(db, "following", user.uid);
+      onSnapshot(ref, (doc) => {
+        setFollowingIds([""]);
+        doc
+          .data()
+          .users.map((user) =>
+            setFollowingIds((prev) => (prev ? [...prev, user.id] : []))
+          );
+      });
+    }
+  }, [user.uid]);
+  useEffect(() => {
+    getFollowingIds();
+  }, [getFollowingIds]);
+
   //게시글 가져오기
   useEffect(() => {
+    let myUnsubscribe;
+    let likeUnsubscribe;
+    let followingUnsubscribe;
     if (user) {
       let momentRef = collection(db, "moment");
       const myMomentQuery = query(
@@ -119,48 +231,91 @@ const Profile = () => {
         where("likes", "array-contains", user.uid),
         orderBy("createdAt", "desc")
       );
+      const followingMomentQuery = query(
+        momentRef,
+        where("userId", "in", followingIds),
+        orderBy("createdAt", "desc")
+      );
 
-      onSnapshot(myMomentQuery, (snapShot) => {
+      myUnsubscribe = onSnapshot(myMomentQuery, (snapShot) => {
         let dataObj = snapShot.docs.map((doc) => ({
           ...doc.data(),
           id: doc?.id,
         }));
         setMyMoments(dataObj);
       });
-      onSnapshot(likeMomentQuery, (snapShot) => {
+      likeUnsubscribe = onSnapshot(likeMomentQuery, (snapShot) => {
         let dataObj = snapShot.docs.map((doc) => ({
           ...doc.data(),
           id: doc?.id,
         }));
         setLikeMoment(dataObj);
       });
+
+      followingUnsubscribe = onSnapshot(followingMomentQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setFollowingMoments(dataObj);
+      });
     }
-  }, [user]);
+    return () => {
+      myUnsubscribe && myUnsubscribe();
+      likeUnsubscribe && likeUnsubscribe();
+      followingUnsubscribe && followingUnsubscribe();
+    };
+  }, [user, activeTab]);
 
   return (
     <Wrap>
       <ProfileWrap>
         <LeftInfo>
-          <Img src={userPhoto ? userPhoto : url} />
-          <label htmlFor="avatar">프로필 이미지 변경</label>
-          <input
-            onChange={onChangeAvatar}
-            id="avatar"
-            type="file"
-            accept="image/*"
-          />
-          <Info>
-            <form onSubmit={onSubmit}>
-              {!isEdit && <span>{displayName}</span>}
-              {isEdit && <input value={newDisplayName} onChange={onChange} />}
-              <span onClick={() => setIsEdit((prev) => !prev)}>
-                {isEdit ? "취소" : "닉네임 수정"}
-              </span>
-              {isEdit && <button>저장</button>}
-            </form>
-          </Info>
+          <ProfileInfo>
+            <img src={userPhoto ? userPhoto : url} />
+            <p>{displayName}</p>
+            <button>프로필 수정하기</button>
+          </ProfileInfo>
+          <Title>내가 기록한 순간들✨</Title>
+          <ProfileMoment>
+            {myMoments.map((moment) => (
+              <ProfileMomentBox moment={moment} key={moment.id} />
+            ))}
+          </ProfileMoment>
         </LeftInfo>
         {/* 내가 작성한 게시글 보기 */}
+        <RightInfo>
+          <Tab>
+            <p
+              className={activeTab === "like" && classes.active}
+              onClick={() => setActiveTab("like")}
+            >
+              좋아요한 게시글
+            </p>
+            <p
+              className={activeTab === "following" && classes.active}
+              onClick={() => setActiveTab("following")}
+            >
+              팔로우 게시글
+            </p>
+          </Tab>
+          <MomentWrap>
+            {activeTab === "like" && (
+              <>
+                {likeMoment.map((moment) => (
+                  <ProfileMomentBox moment={moment} key={moment.id} />
+                ))}
+              </>
+            )}
+            {activeTab === "following" && (
+              <>
+                {followingMoments.map((moment) => (
+                  <ProfileMomentBox moment={moment} key={moment.id} />
+                ))}
+              </>
+            )}
+          </MomentWrap>
+        </RightInfo>
       </ProfileWrap>
     </Wrap>
   );
